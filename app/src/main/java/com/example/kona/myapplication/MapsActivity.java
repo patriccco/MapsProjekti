0ab94efd8923236461977a3a9edccbce36993bf0
Konsta Jaakkola <konsta.jaakkola@metropolia.fi>
2018-02-01 11:41:25 +0200

    Sijanti saadaan firebasesta

--- app/src/main/java/com/example/kona/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/kona/myapplication/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.kona.myapplication;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,20 +10,57 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public double koord;
+    public double koord2;
+    private static final String TAG = "MyActivity";
+
+    public double getKoord() {
+        return koord;
+    }
+
+    public void setKoord(double koord) {
+        this.koord = koord;
+    }
+
+    public double getKoord2() {
+        return koord2;
+    }
+
+    public void setKoord2(double koord2) {
+        this.koord2 = koord2;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
+
+
 
 
     /**
@@ -34,14 +72,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Koordinaatit");
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null) {
+                    koord = (Double) dataSnapshot.child("K1").getValue();
+                    koord2 = (Double) dataSnapshot.child("K2").getValue();
+                    Log.d(TAG, "Value is: " + getKoord());
+                    Log.d(TAG, "Value is: " + koord2);
+                } else {
+                    koord = 100;
+                }
+
+                // Add a marker in Sydney and move the camera
+                LatLng sydney = new LatLng(koord, koord2);
+                Log.d(TAG, "Value is: " + getKoord());
+                Log.d(TAG, "Value is: " + sydney);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("EI Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 }
