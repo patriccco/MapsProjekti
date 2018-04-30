7d108220882780419289742328ea9608e78ecfb6
Patrick Öhman <ohmanpatrick@gmail.com>
2018-04-30 12:56:33 +0300

    kaikenlaista kivaa

--- app/src/main/java/com/example/kona/myapplication/BuyActivity.java
+++ app/src/main/java/com/example/kona/myapplication/BuyActivity.java
@@ -3,7 +3,25 @@ package com.example.kona.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -11,24 +29,79 @@ import android.view.View;
  */
 public class BuyActivity extends AppCompatActivity {
 
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    public FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    final ArrayList<Item> itemList = new ArrayList<>();
+    private int currItem;
+    private int resId;
+    private TextView itemInfo, priceOfItem;
+    private ImageView imageLarge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_buy);
+        recyclerView = findViewById(R.id.recycler);
+        itemInfo = findViewById(R.id.item_info);
+        imageLarge = findViewById(R.id.item_large);
+        priceOfItem = findViewById(R.id.item_cost);
+        layoutManager = new LinearLayoutManager(this);
+
+        DatabaseReference itemRef = database.getReference("Items");
+        itemRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Item item = new Item();
+
+                    item.name = (String) snapshot.child("Name").getValue();
+                    item.info = (String) snapshot.child("Info").getValue();
+                    item.image = (String) snapshot.child("Image").getValue();
+                    item.type = (String) snapshot.child("Type").getValue();
+
+                    long itemPrice = (long) snapshot.child("Cost").getValue();
+                    item.price = (int) itemPrice;
+                    long itemValue = (long) snapshot.child("Worth").getValue();
+                    item.value = (int) itemValue;
+
+                    //long itemAmount = (long) snapshot.child("Amount").getValue();
+                    //item.amount = (int) itemAmount;
+
+                    String img = item.image;
+                    resId = getResources().getIdentifier(img,"drawable",getPackageName());
+
+                    itemList.add(item);
+                }
+                recyclerView.setLayoutManager(layoutManager);
+                //päästäkää tää adapteri kärsimyksestään
+                mAdapter = new ItemAdapter(itemList, resId, itemInfo, imageLarge);
+                recyclerView.setAdapter(mAdapter);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     /**
      *
      * @param view
-     * This takes user back to MainMenuActivity after pressing button purchase items.
      */
+    public void buyItem(View view) {
+        Transaction transaction = new Transaction();
+        int playerMoney = transaction.getMoney();
+        transaction.addItem(itemList);
 
-    public void confirmBuy(View view) {
-        Intent intent = new Intent(this, MainMenuActivity.class);
-        startActivity(intent);
+        Toast toast= Toast.makeText(getApplicationContext(),
+                "Item bought", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_VERTICAL| Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
     }
-
-
 
     /**
      *
